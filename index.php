9883c352e3f023dfe400acf50f74b606b819fb7f
VitalLogix <VitalLogicFTW@outlook.com>
2015-02-22 15:55:01 +0800

    building menu bar

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+<!DOCTYPE html>
 <?php
- echo '<p>Tuckisbest/p>';
+ echo '';
 ?>
+<html lang="en"> 
+<head> 
+	<title>This is my Website!</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
 
+<body> 
+	<ul id="menu"> 
+		<li class='active'><a href="#">Home</a><li>
+		<li><a href='#'>Collective of Strangers</a></li>
+		<li><a href='#'>About Us</a></li>
+		<li><a href='#'>Contact Us</a></li>
+	</ul>
+</body>
+</html>
