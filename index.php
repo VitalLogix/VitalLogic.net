b2e724112e826b489fd3c1c541978727e69f82d2
VitalLogix <VitalLogicFTW@outlook.com>
2015-02-22 16:00:01 +0800

    fixed errors

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body> 
 	<ul id="menu"> 
-		<li class='active'><a href="#">Home</a><li>
+		<li class='active'><a href="#">Home</a></li>
 		<li><a href='#'>Collective of Strangers</a></li>
 		<li><a href='#'>About Us</a></li>
 		<li><a href='#'>Contact Us</a></li>
