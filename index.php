22593980c744ebe1e278c2c0106db49d4f76ec9c
VitalLogix <VitalLogicFTW@outlook.com>
2015-02-22 17:04:54 +0800

    added css

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 </head>
 
 <body> 
-	<ul id="nav"> 
+	<div id='menu'>
+	<ul> 
 		<li class='active'><a href="#">Home</a></li>
 		<li><a href='#'>Collective of Strangers</a></li>
 		<li><a href='#'>About Us</a></li>
 		<li><a href='#'>Contact Us</a></li>
 	</ul>
+	</div>
 </body>
 </html>
